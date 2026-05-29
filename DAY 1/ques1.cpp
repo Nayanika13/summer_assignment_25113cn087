a1bf50839604d77bbb45b02a495e8f573315346e
Nayanika13 <Nayanikabisht.13@gmail.com>
2026-05-29 20:13:22 +0530

    DAY 1 of my summer assignment

---
+++ DAY 1/ques1.cpp
@@ -0,0 +1,19 @@
+//program to calculate sum of first n natural numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum =0;
+    cout << " Enter a number = ";
+    cin >> n;
+
+    for( int i=1;i<=n;i++)
+    {
+        sum +=i;
+    }
+    cout << "sum of first " << n << 
+    "natural numbers ="<<sum;
+
+    return 0;
+}
