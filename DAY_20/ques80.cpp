73339da5f4f09aa9926e7e9d70f6b2d2db849486
Nayanika13 <Nayanikabisht.13@gmail.com>
2026-06-27 20:19:58 +0530

    DAY 20 OF MY SUMMER ASSIGNMENT

---
+++ DAY_20/ques80.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int rows, cols;
+
+    cout <<"Enter the number of rows and columns:";
+    cin>>rows>>cols;
+
+    int a[100][100];
+
+    cout<<"Enter the elements of the matrix:\n";
+    for (int i=0;i<rows;i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            cin>>a[i][j];
+        }
+    }
+
+    cout <<"Column-wise sum:\n";
+    for (int j = 0; j<cols; j++) 
+    {
+        int sum = 0;
+        for (int i = 0; i < rows; i++) 
+        {
+            sum += a[i][j];
+        }
+        cout <<"Sum of column"<< j + 1 << " =" << sum << endl;
+    }
+    return 0;
+}
