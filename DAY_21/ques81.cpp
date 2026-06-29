56eed702b8c8197fe00cefb4326e7b6ec9405f4b
Nayanika13 <Nayanikabisht.13@gmail.com>
2026-06-29 19:02:11 +0530

    DAY21 OF MY SUMMER ASSIGNMENT

---
+++ DAY_21/ques81.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char str[100];
+    int count = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        count++;
+    }
+
+    cout << "Length=" << count;
+
+    return 0;
+}
